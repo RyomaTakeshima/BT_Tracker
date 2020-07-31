120500309ef1236fab54dd76f45fc04becb4877e
RyomaTakeshima <2020m061@kuas.ac.jp>
2020-07-31 22:58:11 +0900

    Hands-on 6.1

--- app/src/main/java/com/example/task10/LogActivity.java
+++ app/src/main/java/com/example/task10/LogActivity.java
@@ -1,15 +1,27 @@
 package com.example.task10;
 
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class LogActivity extends AppCompatActivity {
+
+    EditText userInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+        userInput = (EditText)findViewById(R.id.LogInputData);
+    }
+
+    public void confirmBT(View view) {
+        Intent toConfirm = new Intent(this, ConfirmActivity.class);
+        toConfirm.putExtra("BT_data", userInput.getText().toString());
+        startActivity(toConfirm);
     }
 }
